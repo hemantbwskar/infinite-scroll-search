8ccf4e012d8200997f348a7b613d056c802e34aa
hemantbwskar <86668867+hemantbwskar@users.noreply.github.com>
2022-03-11 20:57:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -12,110 +12,113 @@ import ScrollButton from "./components/ScrollButton";
 
 function App() {
 
-const [data, setData] = useState([]);
-const [query, setQuery] = useState("landscapes");
-const [page, setPage] = useState(1);
-const [hasMore, setHasMore] = useState(true);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("landscapes");
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
-const client_id = "8-AfSfwFTnK35yfuV6LM7OerBZXTwUBIdxhq3fzYLmA";
-const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}&page=${page}`;
+  const client_id = "8-AfSfwFTnK35yfuV6LM7OerBZXTwUBIdxhq3fzYLmA";
+  const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}&page=${page}`;
 
-const fetchImages = () => {
-  axios
-    .get(fetchUrl, {
-      headers: {},
-    })
-    .then((response) => {
-      setData([...data, ...response.data.results]);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  setPage(page + 1);
-};
+  const fetchImages = () => {
+    axios
+      .get(fetchUrl, {
+        headers: {},
+      })
+      .then((response) => {
+        setData([...data, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setPage(page + 1);
+  };
 
-useEffect(() => {
-  fetchImages();
-}, [query]);
+  useEffect(() => {
+    fetchImages();
+  }, [query]);
 
-const searchImages = (e) => {
-  // if (e.keyCode === 13) {
-    setQuery((e.target.value?e.target.value:"landscapes"));
+  const searchImages = (e) => {
+    // if (e.keyCode === 13) {
+    setQuery((e.target.value ? e.target.value : "landscapes"));
     setData([]);
-  // }
-};
+    // }
+  };
 
 
 
-// const onKeyDown1=(()=> {
-//   const inputvalue=document.getElementById('userInput').value
-//   searchImages(inputvalue)
-// })
+  // const onKeyDown1=(()=> {
+  //   const inputvalue=document.getElementById('userInput').value
+  //   searchImages(inputvalue)
+  // })
 
-const[suggestions,setSuggestions]=useState([])
-const fetchSuggestions=async()=>{
-  const res = await fetch('https://polar-badlands-57668.herokuapp.com/searchTerms')
-  const data = await res.json()
-  console.log(data)
-  return data
-}
-
-useEffect(()=>{
-  const getSuggestions=async()=>{
-    const suggestionsFromServer=await fetchSuggestions()
-    setSuggestions(suggestionsFromServer)
+  const [suggestions, setSuggestions] = useState([])
+  const fetchSuggestions = async () => {
+    const res = await fetch('https://polar-badlands-57668.herokuapp.com/searchTerms')
+    const data = await res.json()
+    console.log(data)
+    return data
   }
 
-   getSuggestions()
- },[])
-// let suggestions=[];
-// const res=fetch('https://polar-badlands-57668.herokuapp.com/searchTerms')
-// suggestions=res.json;
+  useEffect(() => {
+    const getSuggestions = async () => {
+      const suggestionsFromServer = await fetchSuggestions()
+      setSuggestions(suggestionsFromServer)
+    }
 
-// const res = JSON.parse(searchItems)
-// const res = await fetch('https://polar-badlands-57668.herokuapp.com/tasks')
+    getSuggestions()
+  }, [])
+  // let suggestions=[];
+  // const res=fetch('https://polar-badlands-57668.herokuapp.com/searchTerms')
+  // suggestions=res.json;
+
+  // const res = JSON.parse(searchItems)
+  // const res = await fetch('https://polar-badlands-57668.herokuapp.com/tasks')
   return (
-    
+
     <div className="test">
-    <div className="App flex">
-      
-      
-    {/* <As */}
-    {/* {(searchItems)=>searchImages(searchItems)} */}
-    <InputBox
-    type="text"
-    // onKeyDown={(e) => searchImages(e)}
-    // placeholder="Search For Images"
-    searchItems={searchImages}
-    suggestions={suggestions}
-    />
-    <InfiniteScroll
-      dataLength={data.length}
-      next={fetchImages}
-      hasMore={hasMore}
-      loader={<p>Loading more...</p>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>You have seen it all</b>
-        </p>
-      }
-    >
-      <div className="main flex">
-        {data.map((data, key) => (
-          <div className="container" key={key}>
-            <img
-              src={data.urls.small}
-              className="image"
-              alt={data.alt_description}
-            />
-            <h4>{data.alt_description}</h4>
+      <div className="App flex">
+
+
+        {/* <As */}
+        {/* {(searchItems)=>searchImages(searchItems)} */}
+        <InputBox
+          type="text"
+          // onKeyDown={(e) => searchImages(e)}
+          // placeholder="Search For Images"
+          searchItems={searchImages}
+          suggestions={suggestions}
+        />
+        <InfiniteScroll
+          dataLength={data.length}
+          next={fetchImages}
+          hasMore={hasMore}
+          loader={<p>Loading more...</p>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>You have seen it all</b>
+            </p>
+          }
+        >
+          <div className="main flex">
+            {data.map((data, key) => (
+              <div className="container" key={key}>
+                <img
+                  src={data.urls.small}
+                  className="image"
+                  alt={data.alt_description}
+                />
+                <h4>{data.alt_description}
+                <a href={data.urls.small}>
+                  <button className="open-button">Open</button>
+                </a></h4>
+              </div>
+            ))}
           </div>
-        ))}
+        </InfiniteScroll>
+        <ScrollButton />
       </div>
-    </InfiniteScroll>
-    <ScrollButton />
-  </div>
-  </div>
+    </div>
   );
 }
 
